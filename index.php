bf0f5ba86b81b12ca2a7281f8d6271e6c7e25520
alexandre <alexandre.p@codeur.online>
2017-02-27 16:55:43 +0100

    Creation header

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
+<?php 
+	require 'connexion.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Page Accueil</title>
+	<link rel="stylesheet" type="text/css" href="page_accueil.css">
 </head>
 <body>
+	<header>
+		<div class="logo">Meme generator</div>
+		<a href="creationmemes.php">Je créé mon meme</a>
+	</header>
+	<div>
+		<?php 
 
+		?> 
+	</div>
 </body>
 </html>
 
