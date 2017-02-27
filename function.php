e903621586d452475e1b7d3b454f183743de8d39
alexandre <alexandre.p@codeur.online>
2017-02-27 15:20:08 +0100

    Function pour l'upload d'image

---
+++ function.php
@@ -0,0 +1,28 @@
+<?php
+
+    $nom_image = $_FILES['image']['tmp_name'];
+    $image = "imagesUpload/".$_FILES['image']['name'];
+
+    $uploaddir = './';
+    $uploadfile = $uploaddir .basename($_FILES['image']['name']);
+
+    $extension_image = array('image/jpg', 'image/jpeg', 'image/png');
+    $type_image = getimagesize($nom_image);
+
+    if(isset($_POST['submit'])){
+            
+        if($_FILES['image']['error'] > 0){
+            echo "Erreur lors du transfert";
+        }
+        else if($_FILES['image']['size'] > 100000){
+            echo "Ce fichier est trop lourd";
+        }
+        else if(in_array($type_image['mime'], $extension_image)){
+            move_uploaded_file($nom_image,$image);
+    		echo "<img src='$image'/>";
+        }
+        else{
+            echo "Ce fichier n'est pas une image";
+        }
+    }
+?>
