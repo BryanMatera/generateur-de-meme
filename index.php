047fa95c3c767dc7705dfe4b3f1df8810f330579
alexandre <alexandre.p@codeur.online>
2017-02-27 15:20:36 +0100

    Formulaire pour l'upload d'image

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form method="post" action="" enctype="multipart/form-data">
+	<input type="file" name="image"/>
+	<input type="submit" name="submit" value="Envoyer" />
+</form>
+<?php 
+	require 'function.php';
+?>
+</body>
+</html>
